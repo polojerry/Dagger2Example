8edb1d2c59666228b2866121666b90d267dc9fde
polojerry <polojerry4@gmail.com>
2020-05-29 10:00:22 +0300

    Field and Member Variable Injection

--- app/src/main/java/com/polotechnologies/dagger2example/MainActivity.kt
+++ app/src/main/java/com/polotechnologies/dagger2example/MainActivity.kt
@@ -5,14 +5,19 @@ import android.os.Bundle
 import com.polotechnologies.dagger2example.dataClass.Car
 import com.polotechnologies.dagger2example.di.CarComponents
 import com.polotechnologies.dagger2example.di.DaggerCarComponents
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+    @Inject
     lateinit var car: Car
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        car = DaggerCarComponents.create().car
+        val carComponents = DaggerCarComponents.create()
+        carComponents.inject(this)
+        
         car.drive()
         
 
--- app/src/main/java/com/polotechnologies/dagger2example/di/CarComponents.kt
+++ app/src/main/java/com/polotechnologies/dagger2example/di/CarComponents.kt
@@ -1,9 +1,12 @@
 package com.polotechnologies.dagger2example.di
 
+import com.polotechnologies.dagger2example.MainActivity
 import com.polotechnologies.dagger2example.dataClass.Car
 import dagger.Component
 
 @Component
 interface CarComponents {
     val car: Car
+
+    fun inject(mainActivity : MainActivity)
 }
